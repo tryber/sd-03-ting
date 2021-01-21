af6a15d45c2785247e4da0ab8cd1bd17aa511c26
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-21 17:22:40 -0300

    test req1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,7 +1,8 @@
 class Queue:
+
     def __init__(self):
         """Inicialize sua estrutura aqui"""
-        self.__data = []
+        self.__data = ()
 
     def __len__(self):
         """Aqui irá sua implementação"""
@@ -9,11 +10,20 @@ class Queue:
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
-        for i in value:
-            self.__data.append(i)
+        self.__data.insert(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.__data.pop()
 
     def search(self, index):
         """Aqui irá sua implementação"""
+
+        try:
+            if index <= 0 or len(self) <= -1:
+                return None
+        except IndexError:
+            return "indice invalido"
+        else:
+            for i in index:
+                return index[i]
