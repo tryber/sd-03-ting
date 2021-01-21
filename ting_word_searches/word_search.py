415cd7db7c7261ce0d0152a6ef2753c4a16de295
luldelrey <rpgstrikered@gmail.com>
2021-01-21 20:52:34 -0300

    trybe, done :(

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,22 +1,48 @@
 from ting_file_management.queue import Queue
-from ting_file_management.file_process import process
 
 
 def exists_word(word, instance):
     words = []
+    current_line = 1
     for item in instance:
+        current_line = 1
+        word_appears = False
         local_word = {
             "palavra": word,
             "arquivo": item["nome_do_arquivo"],
             "ocorrencias": [],
         }
         for line in item["linhas_do_arquivo"]:
-            if word in line:
-                local_word["ocorrencias"]
+            if word.lower() in line.lower():
+                word_appears = True
+                local_word["ocorrencias"].append({"linha": current_line})
+            current_line += 1
+        if word_appears:
+            words.append(local_word)
+    return words
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    words = []
+    current_line = 1
+    for item in instance:
+        current_line = 1
+        word_appears = False
+        local_word = {
+            "palavra": word,
+            "arquivo": item["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for line in item["linhas_do_arquivo"]:
+            if word.lower() in line.lower():
+                word_appears = True
+                local_word["ocorrencias"].append(
+                    {"linha": current_line, "conteudo": line}
+                )
+            current_line += 1
+        if word_appears:
+            words.append(local_word)
+    return words
 
 
 if __name__ == "__main__":
@@ -26,7 +52,7 @@ if __name__ == "__main__":
             "qtd_linhas": "3",
             "linhas_do_arquivo": [
                 "Acima de tudo,",
-                "é fundamental ressaltar que a adoção de políticas descentralizadoras nos obriga",
+                "é fundamental ressaltar que a adoção de políticas nos obriga",
                 "à análise do levantamento das variáveis envolvidas.",
             ],
         },
@@ -34,11 +60,11 @@ if __name__ == "__main__":
             "nome_do_arquivo": "nome_pedro.txt",
             "qtd_linhas": "1",
             "linhas_do_arquivo": [
-                "Aqui contem um texto que fala sobre um menino pobre chamado Pedro.",
+                "Aqui contem que fala sobre um menino pobre chamado Pedro.",
             ],
         },
     ]
     queue = Queue()
     for elem in test:
         queue.enqueue(elem)
-    exists_word("a", queue)
+    print(exists_word("pedro", queue))
