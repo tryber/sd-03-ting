07cd98e37140af7702a5f1e1fc6b04592f550355
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-21 15:13:27 -0300

    Primeiro commit , teste req1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,26 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.data = list()
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self.data.insert.first(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.data.remove_first()
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        try:
+            for i in index:
+                return self[i]
+
+        except EnvironmentError:
+            if index != i:
+                return "Index Invalido"
