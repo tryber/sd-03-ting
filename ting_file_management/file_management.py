dd4de3f46ba39da4e61ebdc5ee3f6659e4bf9340
William Igor <williamigorsilva@gmail.com>
2021-01-21 20:25:07 -0300

    work in 3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,21 +1,16 @@
 import sys
 
 
-def process_file(file):
-    length = file.readlines()
-    qtd_rows = []
-    for row in length:
-        print(row)
-        qtd_rows.append(row.strip())
-
-    return qtd_rows
-
-
 def txt_importer(path_file):
     if not path_file.endswith(".txt"):
         print("Formato inválido", file=sys.stderr)
     try:
         with open(path_file, "r") as file:
-            return process_file(file)
+            length = file.readlines()
+            qtd_rows = []
+            for row in length:
+                print(row)
+                qtd_rows.append(row.strip())
+            return qtd_rows
     except FileNotFoundError:
         print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+    print(dict)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance:
+        return print("Não há elementos", file=sys.stdout)
 
 
 def file_metadata(instance, position):
