a04211e67178606c7e607b77ff8b3dd844a6ece7
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-21 17:54:09 -0300

    Requisito 1 passando nos testes locais

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,4 +1,4 @@
-from double_node import DoublyNode
+from ting_file_management.double_node import DoublyNode
 
 
 class Queue:
@@ -27,15 +27,27 @@ class Queue:
             element_later_than_removed = value_to_be_removed.next
             self.head.next = element_later_than_removed
             element_later_than_removed.previous = self.head
-            value_to_be_removed.reset()
+            # value_to_be_removed.reset()
             self.__length -= 1
-        return value_to_be_removed
+        return value_to_be_removed.value
 
     def search(self, index):
         value_returned = None
-        if index > self.__length - 1:
-            raise Exception("Index out of range")
+        if index > self.__length - 1 or index < 0:
+            raise IndexError
         value_to_be_returned = self.__get_node_at(index)
         if value_to_be_returned:
             value_returned = DoublyNode(value_to_be_returned.value)
-        return value_returned
+        return value_returned.value
+
+    def is_empty(self):
+        return not self.__length
+
+    def __get_node_at(self, position):
+        value_to_be_returned = None
+        if self.head.next != self.tail:
+            value_to_be_returned = self.head.next
+            while position > 0:
+                value_to_be_returned = value_to_be_returned.next
+                position -= 1
+        return value_to_be_returned
