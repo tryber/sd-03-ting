0f0e52fa9da5e9ce41d7ca8153151906189bda75
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-21 15:35:36 -0300

    Test req1 with  FOR

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,14 +1,16 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
-        self.data = []
+        self.__data = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.__data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
-        self.data.append(value)
+        for i in value:
+            self.__data.append(i)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
