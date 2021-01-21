0dad8a699851171bc142ab9bf55ab68d6833df20
luldelrey <rpgstrikered@gmail.com>
2021-01-21 17:36:39 -0300

    ting file management implemented

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import sys
+from os import path
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write("Formato inválido\n")
+    exists = path.exists(path_file)
+    if not exists:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return
+    with open(path_file) as file:
+        txt_imported = []
+        for text in file.readlines():
+            txt_imported.append(text.replace('\n', ''))
+        return txt_imported
+
+
+if __name__ == "__main__":
+    print(txt_importer('./statics/arquivo_test.txt'))
