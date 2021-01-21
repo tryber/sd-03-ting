420068bf3fab5ab163f7b21e0063d0c1de4e66b1
William Igor <williamigorsilva@gmail.com>
2021-01-21 16:30:01 -0300

    William-ting

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
 class Queue:
+    FIRST_ELEMENT = 0
+
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.insert(self.FIRST_ELEMENT, value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        # remove o elemento da fila
+        if self._data:
+            return self._data.pop()
 
-    def search(self, index):
-        """Aqui irá sua implementação"""
+    def search(self, index, order=None):
+        if index < 0 or index > len(self) - 1:
+            return None
+        if not order:
+            return self._data[index]
+        return self._data[::-1][index]
