449bddd86f6a58417e5ce71c58a342f3d7d14fd9
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-21 17:23:46 -0300

    Implementado o no duplo

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,9 +1,16 @@
+from double_node import DoublyNode
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.head = DoublyNode("HEAD")
+        self.tail = DoublyNode("TAIL")
+        self.head.next = self.tail
+        self.tail.previous = self.head
+        self.__length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.__length
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
