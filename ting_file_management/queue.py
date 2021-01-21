ac8714c617c45b76bbbef1410c386ae6396a912c
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-21 17:31:38 -0300

    queue.py implementado 1a versao

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -13,10 +13,29 @@ class Queue:
         return self.__length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        node_value = DoublyNode(value)
+        node_value.previous = self.tail.previous
+        node_value.next = self.tail
+        node_value.previous.next = node_value
+        self.tail.previous = node_value
+        self.__length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value_to_be_removed = None
+        if not self.is_empty():
+            value_to_be_removed = self.head.next
+            element_later_than_removed = value_to_be_removed.next
+            self.head.next = element_later_than_removed
+            element_later_than_removed.previous = self.head
+            value_to_be_removed.reset()
+            self.__length -= 1
+        return value_to_be_removed
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        value_returned = None
+        if index > self.__length - 1:
+            raise Exception("Index out of range")
+        value_to_be_returned = self.__get_node_at(index)
+        if value_to_be_returned:
+            value_returned = DoublyNode(value_to_be_returned.value)
+        return value_returned
