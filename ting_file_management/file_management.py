e131ecfa7a0fe53ceb2856f83033af60f5a9d628
William Igor <williamigorsilva@gmail.com>
2021-01-21 16:46:27 -0300

    formt invalid

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+def process_file(file):
+    """sua funcao aqui"""
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return ValueError("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            process_file(file)
+    except IOError:
+        print(f"Arquivo {path_file} não encontrado\n")
