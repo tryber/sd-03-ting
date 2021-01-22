4ee00bb1a6c34fae41872fa1f1e13a55907d969d
luis.campos <gucampos845@gmail.com>
2021-01-22 17:33:52 -0300

    Teste requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,10 @@
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise ValueError("Formato inválido")
+        with open(path_file, "r") as file_news:
+            news = file_news.readlines()
+            return [new.strip() for new in news]
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado")
