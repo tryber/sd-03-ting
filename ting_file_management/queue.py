741dcb18db2fc30721334ff27509154d3c9153a3
clayton pereira <cpereiramt@gmail.com>
2021-01-22 19:58:24 -0400

    salvando alteraçoes requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,26 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.items = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.items)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        return self.items.insert(0, value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.items.pop()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        result = []
+        try:
+            result.append(self.items[index + len(self.items) - 1])
+        except IndexError:
+            index
+        except ValueError:
+            index
+        return result
