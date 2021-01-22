9e3712dc96f4c1932e30a775d8f5ed02963b3b4b
potew <potew0@gmail.com>
2021-01-21 23:17:33 -0300

    Update queue.py

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,36 @@
 class Queue:
+    # A fila será implementada sobre uma estrutura de lista.
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        # Método append insere no final da lista, logo não serve
+        return self.data.insert(0, value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        # Sem parâmetro, este método remove o último ítem
+        return self.data.pop()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        try:
+            return self.data[index]
+        except IndexError:
+            return 'Índice inválido'
+
+
+if __name__ == '__main__':
+    fila = Queue()
+    fila.data = [1, 2, 3, 4, 5, 6]
+    print(fila.data)
+    fila.enqueue(7)
+    print(fila.data)
+    fila.enqueue(8)
+    print(fila.data)
+    fila.dequeue()
+    print(fila.data)
+    print(fila.search(2))
+    print(fila.search(15))
+    print(fila.__len__())
