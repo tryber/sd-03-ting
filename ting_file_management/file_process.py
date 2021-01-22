3144a88122387a4db3f8cc7c4736ace58ec68ee0
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-22 16:03:54 -0300

    Funcao remove do arquivo file_process implementada com sucesso

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,36 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def process(path_file, instance=Queue):
+    lines = list()
+    result = dict()
+
+    lines = txt_importer(path_file)
+    if lines is not None:
+        result = {
+            "nome_do_arquivo": f"{path_file}",
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+    else:
+        return None
+    sys.stdout.write(f"'nome_do_arquivo': '{path_file}'")
+    sys.stdout.write(f"'qtd_linhas': {len(lines)}")
+    sys.stdout.write(f"'linhas_do_arquivo': {lines}")
+    return instance.enqueue(result)
+
+
+def remove(instance=Queue):
+    if instance.is_empty():
+        sys.stdout.write("Não há elementos\n")
+    else:
+        result = instance.dequeue()
+        # print(result["nome_do_arquivo"])
+        sys.stdout.write(
+            f"Arquivo {result['nome_do_arquivo']} removido com sucesso\n"
+        )
 
 
 def file_metadata(instance, position):
