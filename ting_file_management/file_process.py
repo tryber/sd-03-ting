206603d9da67ecdc259d55cf15c7389615153d6d
William Igor <williamigorsilva@gmail.com>
2021-01-22 19:23:51 -0300

    COmplet

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -8,13 +8,21 @@ def process(path_file, instance):
         "qtd_linhas": len(txt_importer(path_file)),
         "linhas_do_arquivo": txt_importer(path_file),
     }
+    instance.enqueue(dict)
     print(dict)
 
 
 def remove(instance):
     if not instance:
-        return print("Não há elementos", file=sys.stdout)
+        return print("Não há elementos")
+    path_file = instance.search(0)["nome_do_arquivo"]
+    instance.dequeue()
+    return print(f"Arquivo {path_file} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+        print(data)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
