fc24b43340039404b5939bc6032369e8a9980ff8
nato-re <alvares.renato@hotmail.com>
2021-01-22 18:03:37 -0300

    gg time <3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            return print("Formato inválido", file=sys.stderr)
+        with open(path_file) as file:
+            lines = "".join(list(file)).split("\n")
+            print(lines, file=sys.stderr)
+            return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,38 @@
+def extract_word_occurrence_from_file(word, file, content=None):
+    holder = {
+        "palavra": word,
+        "arquivo": file["nome_do_arquivo"],
+        "ocorrencias": [],
+    }
+    file_lines = file["linhas_do_arquivo"]
+    for line in range(len(file_lines)):
+        print(line)
+        print(file_lines[line])
+        if word.lower() in file_lines[line].lower():
+            occur = {
+                "linha": line + 1,
+            }
+            if content:
+                occur["conteudo"] = file_lines[line]
+            holder["ocorrencias"].append(occur)
+    return holder
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for item in instance:
+        search = extract_word_occurrence_from_file(word, item.value)
+        if search["ocorrencias"]:
+            result.append(search)
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for item in instance:
+        search = extract_word_occurrence_from_file(
+            word, item.value, content=True
+        )
+        if search["ocorrencias"]:
+            result.append(search)
+    return result
