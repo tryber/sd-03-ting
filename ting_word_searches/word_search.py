a1d097d0a8145fe6430bfb67d252f9b1c4ec5e33
Marco Barbosa <marco.meireles.b@gmail.com>
2021-01-22 17:45:50 -0300

    Funcao exists_word implementada com sucesso

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    counter = 0
+    for n in range(len(instance)):
+        counter += 1
+        occours = list()
+        temp = instance.search(n)
+        for line in temp["linhas_do_arquivo"]:
+            if word in line:
+                line_number = counter
+                occours.append({
+                            "linha": line_number,
+                        })
+                print(line_number)
+                result.append(
+                    {
+                        "palavra": f"{word}",
+                        "arquivo": temp["nome_do_arquivo"],
+                        "ocorrencias": occours,
+                    }
+                )
+        return result
 
 
 def search_by_word(word, instance):
