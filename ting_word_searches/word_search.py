29a783621ea488a288b5f3500fbd74e3c0915446
luldelrey <rpgstrikered@gmail.com>
2021-01-22 16:51:58 -0300

    word searcher refactored

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,7 +1,14 @@
 from ting_file_management.queue import Queue
 
 
-def exists_word(word, instance):
+def append_if(local_list, line, content, search):
+    if search:
+        local_list.append({"linha": line, "conteudo": content})
+    else:
+        local_list.append({"linha": line})
+
+
+def word_searcher(word, instance, isSearching):
     words = []
     current_line = 1
     for item in instance:
@@ -15,34 +22,21 @@ def exists_word(word, instance):
         for line in item["linhas_do_arquivo"]:
             if word.lower() in line.lower():
                 word_appears = True
-                local_word["ocorrencias"].append({"linha": current_line})
-            current_line += 1
-        if word_appears:
-            words.append(local_word)
-    return words
-
-
-def search_by_word(word, instance):
-    words = []
-    current_line = 1
-    for item in instance:
-        current_line = 1
-        word_appears = False
-        local_word = {
-            "palavra": word,
-            "arquivo": item["nome_do_arquivo"],
-            "ocorrencias": [],
-        }
-        for line in item["linhas_do_arquivo"]:
-            if word.lower() in line.lower():
-                word_appears = True
-                local_word["ocorrencias"].append(
-                    {"linha": current_line, "conteudo": line}
+                append_if(
+                    local_word["ocorrencias"], current_line, line, isSearching
                 )
             current_line += 1
         if word_appears:
             words.append(local_word)
     return words
+
+
+def exists_word(word, instance):
+    return word_searcher(word, instance, False)
+
+
+def search_by_word(word, instance):
+    return word_searcher(word, instance, True)
 
 
 if __name__ == "__main__":
