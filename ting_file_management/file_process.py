bb5b1fcd45d01f9ca0bab6c45c75035aa36eebec
clayton pereira <cpereiramt@gmail.com>
2021-01-23 21:22:46 -0400

    salvando alteraçes iniciais requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,16 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
 
+
+def process(path_file, instance):
+    file_content = txt_importer(path_file)
+    dict_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content,
+    }
+    print(dict_data)
+    instance.enqueue(dict_data)
+    return dict_data
 
 def remove(instance):
     """Aqui irá sua implementação"""
