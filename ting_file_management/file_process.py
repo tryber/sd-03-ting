514f6fb615c4e1b5c9e02c043f1eb06839445751
clayton pereira <cpereiramt@gmail.com>
2021-01-23 22:05:54 -0400

    salvando alteraçoes no requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,8 +1,11 @@
 from ting_file_management.file_management import txt_importer
-
+import sys
 
 def process(path_file, instance):
     file_content = txt_importer(path_file)
+    for each_queue in range(len(instance)):
+        if instance.search(each_queue)["nome_do_arquivo"] == path_file:
+            return
     dict_data = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(file_content),
@@ -10,12 +13,14 @@ def process(path_file, instance):
     }
     print(dict_data)
     instance.enqueue(dict_data)
-    return dict_data
-    
+
 
 def remove(instance):
     """Aqui irá sua implementação"""
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        instance.search(position)
+    except IndexError:
+        sys.stderr.write("Posição inválida")
