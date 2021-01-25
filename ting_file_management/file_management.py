3c369b6f49630b36d14029879b119b272ab32a54
luis.campos <gucampos845@gmail.com>
2021-01-25 12:23:50 -0300

    stderr requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,10 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     try:
         if not path_file.endswith(".txt"):
-            raise ValueError("Formato inválido")
+            raise ValueError("Formato inválido", file=sys.stderr)
         with open(path_file, "r") as file_news:
             news = file_news.readlines()
             return [new.strip() for new in news]
 
     except FileNotFoundError:
-        print(f"Arquivo {path_file} não encontrado")
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
