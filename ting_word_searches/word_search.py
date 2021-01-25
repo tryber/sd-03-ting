ea814894826b1731390a2576d0f76d4307fd897a
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-25 15:12:48 -0300

    test exists

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -5,6 +5,8 @@ def exists_word(word, instance):
         find_word = instance.search(word)
         if find_word[i]:
             return word_news.append(find_word)
+        else:
+            return ''
 
 
 def search_by_word(word, instance):
