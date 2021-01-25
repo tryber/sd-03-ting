2c1277757ec3d3c4f020e86e07f5f2137fbaf065
luizfernando <luizfernandotic@gmail.com>
2021-01-25 16:31:09 -0300

    requisito 1 enqueue passando nos testes locais

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,13 +1,28 @@
+from ting_file_management.__init__ import DoublyNode
+
+
 class Queue:
     # initial commit
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.head = DoublyNode("HEAD")
+        self.tail = DoublyNode("TAIL")
+        self.head.next = self.tail
+        self.tail.previous = self.head
+        self.__length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.__length
+
+    def is_empty(self):
+        return not self.__length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        node_value = DoublyNode(value)
+        node_value.previous = self.tail.previous
+        node_value.next = self.tail
+        node_value.previous.next = node_value
+        self.tail.previous = node_value
+        self.__length += 1
 
     def dequeue(self):
         """Aqui irá sua implementação"""
