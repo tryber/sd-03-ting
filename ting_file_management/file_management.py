3ff8d912fb36bf849a33655cb100edd412470ed3
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-25 12:23:34 -0300

    req 2 new_error format

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -5,10 +5,10 @@ def txt_importer(path_file):
     """Aqui irá sua implementação"""
     try:
         if not path_file.endswith(".txt"):
-            raise ValueError("Formato inválido")
+            raise ValueError("Formato inválido", file=sys.stderr)
         with open(path_file, "r") as file_news:
             news = file_news.readlines()
             return [new.strip() for new in news]
 
     except FileNotFoundError:
-        print(f"Arquivo {path_file} não encontrado", file_news=sys.stderr)
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
