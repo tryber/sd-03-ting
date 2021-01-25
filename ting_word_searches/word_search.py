05cfab5813a65a338975ef12cd6d54c4ad45bc2e
luis.campos <gucampos845@gmail.com>
2021-01-25 15:05:52 -0300

    Exists word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    new_list = []
+
+    for node in instance:
+        lines = node.value["linhas_do_arquivo"]
+        new_dict = {
+            "palavra": word,
+            "arquivo": node.value["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for i, line in enumerate(lines):
+            line_change = line.replace(".", "")
+            line_split = line_change.split()
+            if word in line_split:
+                new_dict["ocorrencias"].append({"linha": i + 1})
+
+        if len(new_dict["ocorrencias"]) > 0:
+            new_list.append(new_dict)
+
+    return new_list
 
 
 def search_by_word(word, instance):
