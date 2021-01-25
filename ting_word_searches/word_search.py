59708253eda50a89a08ab16fe0055149d3f14b08
William Igor <williamigorsilva@gmail.com>
2021-01-25 15:16:11 -0300

    I guess i finish all

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,8 +1,45 @@
+# https://docs.python.org/3/library/re.html regex python
+import re
+
+
 def exists_word(word, instance):
-    list = list()
-    if not word:
-        return  list
+    lines = 0
+    result = []
+    ocorrences = []
+    for words in range(len(instance)):
+        lines += 1
+        temp = instance.search(words)
+        for line in temp["linhas_do_arquivo"]:
+            if re.findall(word, line, re.IGNORECASE):
+                line_number = lines
+                ocorrences.append({"linha": line_number})
+                result.append({
+                    "palavra": f"{word}",
+                    "arquivo": temp["nome_do_arquivo"],
+                    "ocorrencias": ocorrences
+                })
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    ocorrences = []
+    lines = 0
+    for worlds in range(len(instance)):
+        lines += 1
+        temp = instance.search(worlds)
+        for line in temp["linhas_do_arquivo"]:
+            if re.findall(word, line, re.IGNORECASE):
+                line_number = lines
+                ocorrences.append({
+                            "linha": line_number,
+                            "conteudo": f"{line}"
+                        })
+                result.append(
+                    {
+                        "palavra": f"{word}",
+                        "arquivo": temp["nome_do_arquivo"],
+                        "ocorrencias": ocorrences,
+                    }
+                )
+        return result
