a39c84bbf8ec759164fc7607218c1d671e9a1e11
luizfernando <luizfernandotic@gmail.com>
2021-01-25 17:13:05 -0300

    requisito 2 dequeue e search passando nos testes locais

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -25,7 +25,29 @@ class Queue:
         self.__length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value_to_be_removed = None
+        if not self.is_empty():
+            value_to_be_removed = self.head.next
+            element_later_than_removed = value_to_be_removed.next
+            self.head.next = element_later_than_removed
+            element_later_than_removed.previous = self.head
+            self.__length -= 1
+        return value_to_be_removed.value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        value_returned = None
+        if index > self.__length - 1 or index < 0:
+            raise IndexError
+        value_to_be_returned = self.__get_node_at(index)
+        if value_to_be_returned:
+            value_returned = DoublyNode(value_to_be_returned.value)
+        return value_returned.value
+
+    def __get_node_at(self, position):
+        value_to_be_returned = None
+        if self.head.next != self.tail:
+            value_to_be_returned = self.head.next
+            while position > 0:
+                value_to_be_returned = value_to_be_returned.next
+                position -= 1
+        return value_to_be_returned
