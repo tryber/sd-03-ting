64044dbad16122774696db520aaacf4b492cfb83
rodrigo-militao <militaorodrigo9@gmail.com>
2021-01-25 15:33:42 -0300

    Last project done.. Thank you trybe for everything. I loved each moment

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,48 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = list()
+    result = dict()
+
+    # verifica se o arquivo já existe na fila
+    for n in range(len(instance)):
+        check_list = instance.search(n)
+        if check_list["nome_do_arquivo"] == f"{path_file}":
+            return None
+
+    # se o arquivo nao existe, carregue-o
+    lines = txt_importer(path_file)
+    if lines is not None:
+        result = {
+            "nome_do_arquivo": f"{path_file}",
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+    else:
+        return None
+
+    # coloque o arquivo na fila e imprima as informacoes
+    instance.enqueue(result)
+    sys.stdout.write(f"'nome_do_arquivo': '{path_file}'")
+    sys.stdout.write(f"'qtd_linhas': {len(lines)}")
+    sys.stdout.write(f"'linhas_do_arquivo': {lines}")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.is_empty():
+        sys.stdout.write("Não há elementos\n")
+    else:
+        result = instance.dequeue()
+        sys.stdout.write(
+            f"Arquivo {result['nome_do_arquivo']} removido com sucesso\n"
+        )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position < 0 or position > len(instance):
+        sys.stderr.write("Posição inválida")
+        return None
+    result = instance.search(position)
+    sys.stdout.write(f"{result}")
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,50 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    counter = 0
+    for n in range(len(instance)):
+        counter += 1
+        occours = list()
+        temp = instance.search(n)
+        for line in temp["linhas_do_arquivo"]:
+            if re.search(word, line, re.IGNORECASE):
+                line_number = counter
+                occours.append({
+                            "linha": line_number,
+                        })
+                print(line_number)
+                result.append(
+                    {
+                        "palavra": f"{word}",
+                        "arquivo": temp["nome_do_arquivo"],
+                        "ocorrencias": occours,
+                    }
+                )
+        return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    counter = 0
+    for n in range(len(instance)):
+        counter += 1
+        occours = list()
+        temp = instance.search(n)
+        for line in temp["linhas_do_arquivo"]:
+            if re.search(word, line, re.IGNORECASE):
+                line_number = counter
+                occours.append({
+                            "linha": line_number,
+                            "conteudo": f"{line}"
+                        })
+                print(line_number)
+                result.append(
+                    {
+                        "palavra": f"{word}",
+                        "arquivo": temp["nome_do_arquivo"],
+                        "ocorrencias": occours,
+                    }
+                )
+        return result
