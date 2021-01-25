706c43b3966befbd9a1b6a9241f8ae8697931c43
luis.campos <gucampos845@gmail.com>
2021-01-25 13:56:01 -0300

    Erro file not found

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,13 +1,15 @@
 import sys
+from os import path
 
 
 def txt_importer(path_file):
-    try:
-        if not path_file.endswith(".txt"):
-            raise ValueError("Formato inválido", file=sys.stderr)
-        with open(path_file, "r") as file_news:
-            news = file_news.readlines()
-            return [new.strip() for new in news]
+    path_valide = path.exists(path_file)
 
-    except FileNotFoundError:
+    if not path_valide:
         return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    if not path_file.endswith(".txt"):
+        raise ValueError("Formato inválido", file=sys.stderr)
+    with open(path_file, "r") as file_news:
+        news = file_news.readlines()
+        return [new.strip() for new in news]
