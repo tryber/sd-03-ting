c84c811a7377b1bd78cbcb94713686bdb65d84fa
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-25 15:12:15 -0300

    test req 6 exists word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,10 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    word_news = []
+    for i in instance:
+        find_word = instance.search(word)
+        if find_word[i]:
+            return word_news.append(find_word)
 
 
 def search_by_word(word, instance):
