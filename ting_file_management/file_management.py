f4407bbb3653af86845c3fd74c455fb334e423e2
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-25 14:05:47 -0300

    recover test2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,11 +3,12 @@ import sys
 
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
-    if not path_file.endswith(".txt"):
-        raise ValueError("Formato inválido", file=sys.stderr)
-    if path_file.exists(path_file) is False:
-        raise ValueError(
-            f"Arquivo {path_file.basename(path_file)} não encontrado")
-    with open(path_file, "r") as file_news:
-        news = file_news.readlines()
-        return [new.strip() for new in news]
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+        with open(path_file) as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines]
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
