dfa82e2303f7d2b160acda64a51634df129cbd39
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2021-01-25 16:15:31 -0300

    req6 exists

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,16 +1,27 @@
+import re
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
-    n_list = []
-    seach_word = instance.search()
+    word_list = []
+    lines = 0
+
     for i in range(len(instance)):
-        if word != seach_word(["nome_do_arquivo"]):
-            return ''
-        else:
-            n_list.append({
-                "palavra": f"{word}",
-                "arquivo": seach_word["nome_do_arquivo"],
-                "ocorrencias": instance,
-            })
+        seach_intance = instance.search(i)
+        list_of_ocorrence = []
+        lines += 1
+        for line in seach_intance["linhas_do_arquivo"]:
+            if re.search(word, line, re.IGNORECASE):
+                list_of_ocorrence.append({
+                    "linha": lines,
+                })
+                word_list.append({
+                    "palavra": f"{word}",
+                    "arquivo": seach_intance["nome_do_arquivo"],
+                    "ocorrencias": list_of_ocorrence,
+                })
+
+    return word_list
 
 
 def search_by_word(word, instance):
