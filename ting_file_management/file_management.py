eeaa7bf8554704d31a8cf3a2b0088cef3696572f
tales <talestales@gmail.com>
2021-01-26 18:32:39 -0300

    a

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file) as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines]
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    txt_importer("statics/arquivo_teste.txt")
