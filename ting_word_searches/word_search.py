ee039cefec69ae7a71973d73179ec67280fd6485
luizfernando <luizfernandotic@gmail.com>
2021-01-27 18:59:36 -0300

    requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    lines = 0
+    result = []
+    ocorrences = []
+    for words in range(len(instance)):
+        lines += 1
+        temp = instance.search(words)
+        for line in temp["linhas_do_arquivo"]:
+            if re.findall(word, line, re.IGNORECASE):
+                line_number = lines
+                ocorrences.append({"linha": line_number})
+                result.append(
+                    {
+                        "palavra": f"{word}",
+                        "arquivo": temp["nome_do_arquivo"],
+                        "ocorrencias": ocorrences,
+                    }
+                )
+    return result
 
 
 def search_by_word(word, instance):
