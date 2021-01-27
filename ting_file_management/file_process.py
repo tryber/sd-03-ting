9169c9bbee5275016f95d5abb1618643d42f9590
Henrique Rezende <henriquerezendec@gmail.com>
2021-01-27 20:58:42 -0300

    Req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    txt_file = txt_importer(path_file)
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_file),
+        "linhas_do_arquivo": txt_file
+    }
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
