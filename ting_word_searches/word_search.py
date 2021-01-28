57ec8dd59c40f557bf2f5ea1d9809ade6d9864f2
Cardoso <lsc.cardosoo@gmail.com>
2021-01-27 22:31:10 -0300

    exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,30 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list_of_occurrences = []
+    occurrences = []
+    for i in range(instance.__len__()):
+        for j in range(len(instance.search(i)['linhas_do_arquivo'])):
+            if (
+                re.search(word, instance.search(i)['linhas_do_arquivo'][j],
+                          re.IGNORECASE)):
+                occurrences.append({
+                    "linha": j + 1,
+                    "conteudo": instance.search(i)['linhas_do_arquivo'][j]
+                    .strip(),
+                })
+
+        if len(occurrences) > 0:
+            list_of_occurrences.append({
+                "palavra": word,
+                "arquivo": instance.search(i)['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+            occurrences = []
+
+    return list_of_occurrences
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
