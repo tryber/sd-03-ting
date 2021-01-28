1a4a96f51a64c7e9a23e03555494fba5a967d083
Cardoso <lsc.cardosoo@gmail.com>
2021-01-27 21:18:30 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_extension = os.path.basename(path_file)
+    if (not file_extension.endswith('.txt')):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, 'r') as file:
+            file_list = list()
+            file_lines = file.readlines()
+            for line in file_lines:
+                line = line.strip()
+                file_list.append(line)
+            return file_list
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
