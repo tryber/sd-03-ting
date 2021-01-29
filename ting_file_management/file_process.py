14cf4e7bb3c589e69502e00200972c81f2af5840
fabemiliano <fab.emiliano@gmail.com>
2021-01-28 21:36:30 -0300

    all good

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,28 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+    instance.enqueue(dict)
+    print(dict)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance:
+        return print("Não há elementos", file=sys.stdout)
+        path_file = instance.search(0)["nome_do_arquivo"]
+        instance.dequeue()
+        print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+        print(data, file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
