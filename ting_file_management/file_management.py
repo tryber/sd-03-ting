45499e70bbb5b4de73903dfddfec245c60434aaf
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-01-30 12:20:24 -0300

    Passing locally test_file_mangement

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+        with open(path_file, 'r') as file:
+            lines = file.readlines()
+            lines_array = []
+            for line in lines:
+                lines_array.append(line.strip())
+            return lines_array
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
