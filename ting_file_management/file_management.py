43dab87cddb30151d942596ba547b60b74f210d5
Hfreitas <hfreitas86@gmail.com>
2021-01-31 17:55:26 -0300

    requisito 2 ok

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+from os import path
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path.exists(path_file):
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    with open(path_file, "r", encoding="UTF-8") as file:
+        content = []
+        for line in file:
+            content.append(line.replace("\n", ""))
+        return content
