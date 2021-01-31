c9cad2ef582cb8c586fdc6d53e6602553bb51299
Hfreitas <hfreitas86@gmail.com>
2021-01-31 18:19:02 -0300

    verificando se o item já existe na fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,6 +3,11 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    # verifica se o item já foi gravado na fila
+    for item in range(len(instance)):
+        if instance.search(item)["nome_do_arquivo"] == path_file:
+            return
+
     file_info = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(txt_importer(path_file)),
