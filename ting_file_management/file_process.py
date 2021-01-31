11eb4aaca99d63294a0287c18d8bc4f09b36113a
Hfreitas <hfreitas86@gmail.com>
2021-01-31 18:09:32 -0300

    correção em key de info do arquivo

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,15 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+    instance.enqueue(file_info)
+    print(file_info)
 
 
 def remove(instance):
