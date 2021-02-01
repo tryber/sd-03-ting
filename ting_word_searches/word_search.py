38e16bedc18c23a8f2125c0fc8e62450faf6b893
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-02-01 20:10:39 -0300

    Requisito 5 e 6 - word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,44 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = instance._data
+    result = []
+    occurrences = []
+    line = 0
+    for file in data:
+        for phrase in file["linhas_do_arquivo"]:
+            if re.findall(word, phrase, re.IGNORECASE):
+                line += 1
+                occurrences.append({"linha": line})
+        if len(occurrences):
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = instance._data
+    result = []
+    occurrences = []
+    line = 0
+    for file in data:
+        for phrase in file["linhas_do_arquivo"]:
+            if re.findall(word, phrase, re.IGNORECASE):
+                line += 1
+                occurrences.append({"linha": line, "conteudo": phrase})
+                print(occurrences)
+        if len(occurrences):
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return result
