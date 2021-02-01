4c82688651e922dd732368a17881b6caaca2f1b7
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-02-01 15:39:50 -0300

    Requisito 2 - txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    txt_list = []
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+        with open(path_file, "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                txt_list.append(line.strip())
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        return txt_list
