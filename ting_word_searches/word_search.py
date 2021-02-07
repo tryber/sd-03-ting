9360ac9cb4cb28a22519c6167cfd0ee669da5303
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2021-02-07 16:22:57 -0300

    Separating word_search in two functions due to complexity

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,16 +1,11 @@
-def find_word(word, instance, function):
+def exists_word(word, instance):
     word_list = []
     for i in range(len(instance)):
         search = instance.search(i)
         word_occurence = []
         for index, line in enumerate(search['linhas_do_arquivo']):
             if word.lower() in line.lower():
-                if function == 'exists':
-                    word_occurence.append({"linha": index + 1})
-                else:
-                    word_occurence.append(
-                        {"linha": index + 1, "conteudo": line}
-                    )
+                word_occurence.append({"linha": index + 1})
         if word_occurence == []:
             return word_occurence
         word_list.append({
@@ -21,9 +16,21 @@ def find_word(word, instance, function):
         return word_list
 
 
-def exists_word(word, instance):
-    return find_word(word, instance, 'exists')
-
-
 def search_by_word(word, instance):
-    return find_word(word, instance, 'search')
+    word_list = []
+    for i in range(len(instance)):
+        search = instance.search(i)
+        word_occurence = []
+        for index, line in enumerate(search['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                word_occurence.append(
+                    {"linha": index + 1, "conteudo": line}
+                )
+        if word_occurence == []:
+            return word_occurence
+        word_list.append({
+            "arquivo": search['nome_do_arquivo'],
+            "ocorrencias": word_occurence,
+            "palavra": word
+        })
+        return word_list
