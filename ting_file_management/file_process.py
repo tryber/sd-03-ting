483c17616c833131547fcff7a9d698ab37852e26
wendersonbelko <contato@wendersonbelko.com>
2021-02-08 19:54:44 -0300

    ultimo <3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,35 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    """ print(data) """
+    info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+    print(info)
+    for i in instance:
+        if i.value == info:
+            info = {}
+
+    info and instance.enqueue(info)
+
+    return info
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):
+        print("Não há elementos")
+    else:
+        info = instance.dequeue()
+        print(f"Arquivo {info['nome_do_arquivo']} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if len(instance) <= position:
+        print("Posição inválida", file=sys.stderr)
+    else:
+        return instance.search(position)
