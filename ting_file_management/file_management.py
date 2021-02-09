a71117c564b370c589593d80d4499ddce7c53d03
Mateus Talles <mateustalles@gmail.com>
2021-02-09 18:21:04 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import re
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not re.search('.txt$', path_file):
+            return sys.stderr.write("Formato inválido\n")
+        with open(path_file, "r", encoding="utf-8") as file:
+            text = ""
+            for line in file:
+                text += line
+            text = text.split("\n")
+            print(text)
+        return text
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
+if __name__ == "__main__":
+    txt_importer("../statics/arquivo_teste.txt")
