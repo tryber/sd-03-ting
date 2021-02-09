30bceca0f1f77497c002a682fd1999fee4c0bb34
gabryelryba <gabryel.ryba@gmail.com>
2021-02-09 17:41:59 -0300

    process calling enqueue method

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def format_data(name, lines):
+    return {
+        "nome_do_arquivo": name,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text_data = txt_importer(path_file)
+    for i in range(instance.__len__()):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return
+
+    return_dict = format_data(path_file, text_data)
+    instance.enqueue(return_dict)
+    print(return_dict, file=sys.stdout)
 
 
 def remove(instance):
