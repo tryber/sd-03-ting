18e9d11b1464ebef6ef3197d78ed393e3e00ec2e
rdgbrezolin <brezolinr@gmail.com>
2021-02-09 15:47:13 -0300

    Obg Trybe!

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,15 +3,14 @@ import sys
 
 def txt_importer(path_file):
     if not path_file.endswith('.txt'):
-        return print("Formato inválido\n", file=sys.stderr)
+        print('Formato inválido', file=sys.stderr)
     try:
-        with open(path_file, 'r', encoding='UTF-8') as file:
-            lines = file.readlines()
+        with open(path_file) as value:
+            lines = value.readlines()
             return [line.strip() for line in lines]
-
     except FileNotFoundError:
         print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
 
 
-if __name__ == "__main__":
-    txt_importer("statics/arquivo_teste.txt")
+if __name__ == '__main__':
+    txt_importer('statics/arquivo_teste.txt')
