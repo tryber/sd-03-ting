b4d87e950f557b5049951e5e91ec5646b9a25040
Rhian Castro <rhi.castro@gmail.com>
2021-02-09 19:28:47 -0300

    file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+from os import path
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if not path.exists(path_file):
+            print("Formato inválido")
+
+        with open(path_file, "r") as file:
+            text_data = []
+            text_lines = file.readlines()
+
+            for line in text_lines:
+                text_data.append(line.strip())
+            return text_data
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado")
