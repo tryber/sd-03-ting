1a375811a408d8565d3cf09b57ebedae745e6c3c
gabryelryba <gabryel.ryba@gmail.com>
2021-02-09 17:20:58 -0300

    queue methods

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,15 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        return self._queue[index]
