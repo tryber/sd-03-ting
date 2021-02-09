717e650e042c3ab49d2cd82cf8ba83d80a2eb7f5
gabryelryba <gabryel.ryba@gmail.com>
2021-02-09 18:15:41 -0300

    word_search and search_by_word passing all tests

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,43 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue_list = instance._queue
+    matches = []
+    occurrences = []
+    line_count = 0
+
+    for file in queue_list:
+        for phrase in file["linhas_do_arquivo"]:
+            if re.findall(word, phrase, re.IGNORECASE):
+                line_count += 1
+                occurrences.append({"linha": line_count})
+
+        if len(occurrences):
+            matches.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences})
+
+    return matches
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue_list = instance._queue
+    matches = []
+    occurrences = []
+    line_count = 0
+
+    for file in queue_list:
+        for phrase in file["linhas_do_arquivo"]:
+            if re.findall(word, phrase, re.IGNORECASE):
+                line_count += 1
+                occurrences.append({"linha": line_count, "conteudo": phrase})
+
+        if len(occurrences):
+            matches.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences})
+
+    return matches
