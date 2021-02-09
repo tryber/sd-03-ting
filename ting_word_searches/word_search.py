9c3265c2bcfb33bdc4bf1d10aeac8e0c55b03c1b
rdgbrezolin <brezolinr@gmail.com>
2021-02-09 16:22:34 -0300

    Obg Trybe!

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,71 +1,71 @@
-def search_word_sentence(palavra_procurada, value):
-    lines = []
-    for line_index, sentence in enumerate(value):
+def search_word_sentence(searched_word, sentences):
+    value1 = []
+    for line_index, sentence in enumerate(sentences):
         for word in sentence.split(" "):
-            if word.lower().replace(".", "") == palavra_procurada.lower():
-                lines.append(
+            if word.lower().replace(".", "") == searched_word.lower():
+                value1.append(
                     {
                         "linha": line_index + 1,
                         "conteudo": sentence,
                     }
                 )
 
-    return lines
+    return value1
 
 
-def search_word_new(palavra_procurada, value):
-    lines = []
-    for i, sentence in enumerate(value):
+def search_new_word(searched_word, sentences):
+    value1 = []
+    for line_index, sentence in enumerate(sentences):
         for word in sentence.split(" "):
-            if word.lower().replace(".", "") == palavra_procurada.lower():
-                lines.append({"linha": i + 1})
+            if word.lower().replace(".", "") == searched_word.lower():
+                value1.append({"linha": line_index + 1})
 
-    return lines
-
-
-def exists_word(word, instance):
-    news = []
-
-    for i_news in range(len(instance)):
-        foundFile = instance.dequeue()
-        matched_lines = search_word_new(word, foundFile["linhas_do_arquivo"])
-        if matched_lines:
-            news.append(
-                {
-                    "palavra": word,
-                    "arquivo": foundFile["nome_do_arquivo"],
-                    "ocorrencias": matched_lines,
-                }
-            )
-        instance.enqueue(foundFile)
-
-    return news
+    return value1
 
 
 def search_by_word(word, instance):
-    news = []
+    value2 = []
     for i_news in range(len(instance)):
-        foundFile = instance.dequeue()
+        lines_arquivo = instance.dequeue()
         matched_lines = search_word_sentence(
-            word, foundFile["linhas_do_arquivo"]
+            word, lines_arquivo["linhas_do_arquivo"]
         )
         if matched_lines:
-            news.append(
+            value2.append(
                 {
                     "palavra": word,
-                    "arquivo": foundFile["nome_do_arquivo"],
+                    "arquivo": lines_arquivo["nome_do_arquivo"],
                     "ocorrencias": matched_lines,
                 }
             )
-        instance.enqueue(foundFile)
+        instance.enqueue(lines_arquivo)
 
-    return news
+    return value2
+
+
+def exists_word(word, instance):
+    value2 = []
+    for i_news in range(len(instance)):
+        lines_arquivo = instance.dequeue()
+        verify = search_new_word(word, lines_arquivo["linhas_do_arquivo"])
+        if verify:
+            value2.append(
+                {
+                    "palavra": word,
+                    "arquivo": lines_arquivo["nome_do_arquivo"],
+                    "ocorrencias": verify,
+                }
+            )
+        instance.enqueue(lines_arquivo)
+
+    return value2
 
 
 if __name__ == "__main__":
     from ting_file_management.queue import Queue
     from ting_file_management.file_process import process
 
-    filona = Queue()
-    process("statics/nome_pedro.txt", filona)
-    word = search_by_word("pedro", filona)
+    project = Queue()
+    process("statics/nome_pedro.txt", project)
+    word = search_by_word("pedro", project)
+    print(word)
