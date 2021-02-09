7f7c45ce69377fa78d86037c4635f9e9b5e4fe1c
gabryelryba <gabryel.ryba@gmail.com>
2021-02-09 16:59:02 -0300

    filenotfound exception at txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,12 +2,17 @@ import sys
 
 
 def txt_importer(path_file):
-    if not path_file.endswith(".txt"):
-        print("Formato inválido", file=sys.stderr)
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
 
-    with open(path_file, "r") as file:
-        text_data = []
-        text_lines = file.readlines()
-        for line in text_lines:
-            text_data.append(line.strip())
-        return text_data
+        with open(path_file, "r") as file:
+            text_data = []
+            text_lines = file.readlines()
+
+            for line in text_lines:
+                text_data.append(line.strip())
+            return text_data
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
