d773fc7f40b55684740ed5c591e0a38a25057fb7
gabryelryba <gabryel.ryba@gmail.com>
2021-02-09 16:57:41 -0300

    txt_importer working locally

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    with open(path_file, "r") as file:
+        text_data = []
+        text_lines = file.readlines()
+        for line in text_lines:
+            text_data.append(line.strip())
+        return text_data
