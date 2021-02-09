10c5354f7780f974c8a4059426d405d9d5afb33c
rdgbrezolin <brezolinr@gmail.com>
2021-02-09 17:36:13 -0300

    Obg Trybe!

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,3 +1,12 @@
+def procura_nova(palavra, sentences):
+    value1 = []
+    for line_index, sentence in enumerate(sentences):
+        for word in sentence.split(" "):
+            if word.lower().replace(".", "") == palavra.lower():
+                value1.append({"linha": line_index + 1})
+    return value1
+
+
 def procura_palavra(palavra, sentences):
     value1 = []
     for line_index, sentence in enumerate(sentences):
@@ -9,17 +18,6 @@ def procura_palavra(palavra, sentences):
                         "conteudo": sentence,
                     }
                 )
-
-    return value1
-
-
-def search_new_word(palavra, sentences):
-    value1 = []
-    for line_index, sentence in enumerate(sentences):
-        for word in sentence.split(" "):
-            if word.lower().replace(".", "") == palavra.lower():
-                value1.append({"linha": line_index + 1})
-
     return value1
 
 
@@ -39,7 +37,6 @@ def search_by_word(palavra, instance):
                 }
             )
         instance.enqueue(lines_arquivo)
-
     return value2
 
 
@@ -47,7 +44,7 @@ def exists_word(palavra, instance):
     value2 = []
     for i_news in range(len(instance)):
         lines_arquivo = instance.dequeue()
-        verify = search_new_word(palavra, lines_arquivo["linhas_do_arquivo"])
+        verify = procura_nova(palavra, lines_arquivo["linhas_do_arquivo"])
         if verify:
             value2.append(
                 {
@@ -57,15 +54,4 @@ def exists_word(palavra, instance):
                 }
             )
         instance.enqueue(lines_arquivo)
-
     return value2
-
-
-if __name__ == "__main__":
-    from ting_file_management.queue import Queue
-    from ting_file_management.file_process import process
-
-    project = Queue()
-    process("statics/nome_pedro.txt", project)
-    palavra = search_by_word("pedro", project)
-    print(palavra)
