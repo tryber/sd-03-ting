20c8dd441471b2933cf3ff4bfca5c95e996eb216
rdgbrezolin <brezolinr@gmail.com>
2021-02-09 16:39:38 -0300

    Obg Trybe!

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,6 +5,16 @@ from ting_file_management.file_management import txt_importer
 
 # Implementa arquivo no fila
 @cache
+def remove(instance):
+    if instance.__len__() > 0:
+        file = instance.dequeue()
+        print(
+            f"Arquivo {file['nome_do_arquivo']} removido com sucesso"
+        )
+    else:
+        print("Não há elementos")
+
+
 def process(path_file, instance):
     values = txt_importer(path_file)
     processed_data = {
@@ -15,13 +25,6 @@ def process(path_file, instance):
     instance.enqueue(processed_data)
     print(processed_data, file=sys.stdout)
     return processed_data
-
-
-def remove(instance):
-    if not len(instance):
-        sys.stdout.write('Não há elementos\n')
-    name_file = instance.dequeue()['nome_do_arquivo']
-    print(f'Arquivo {name_file} removido com sucesso')
 
 
 def file_metadata(instance, position):
