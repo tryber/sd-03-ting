8612a0aafedf3e2d704bf443b152cc841f80f03c
Mateus Talles <mateustalles@gmail.com>
2021-02-09 20:10:41 -0300

    word searches

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,51 @@
+# from .content_deque import Deque
+from operator import itemgetter
+import sys
+# sys.path.insert(1, '../ting_file_management')
+# from file_process import process
+# from file_management import txt_importer
+# from queue import Queue
+
+
+def phrase_to_words(phrase):
+    alphabet = "abcdefghijklmnopqrstuvwyxzàáèéìíòóùúäëïöü "
+    phrase = "".join(str(x).lower() for x in phrase)
+    for letter in phrase:
+        if letter.lower() not in alphabet:
+            phrase = phrase.replace(letter, "")
+    phrase = phrase.split(" ")
+    return phrase
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    for i in range(len(instance)):
+        if instance.search(i):
+            process_metada = instance.search(i)
+            file_path, lines_qty, data = itemgetter(
+                "nome_do_arquivo",
+                "qtd_linhas",
+                "linhas_do_arquivo"
+            )(process_metada)
+
+            words = [
+                { "linha": index + 1 }
+                for index, phrase in enumerate(data)
+                if word in phrase_to_words(phrase)
+            ]
+
+            result.append({
+                "palavra": word,
+                "arquivo": file_path,
+                "ocorrencias": [*words]
+            })
+    print(result)
+    return result
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+# if __name__ == "__main__":
+#     instance = Queue()
+#     process("../statics/arquivo_teste.txt", instance)
+#     exists_word("é", instance)
