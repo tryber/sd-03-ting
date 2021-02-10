7e8b40b17361fd23c547f18ec47d11f7c728a58d
DiegoRafaelAF <draf0314@gmail.com>
2021-02-09 23:13:47 -0300

    16 testes passando localmente

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação """
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    result = list()
+    try:
+        with open(path_file, "r", newline="") as txt_file:
+            for news in txt_file.readlines():
+                result.append(news.rstrip('\n'))
+        return result
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
