309b85279072eaf9287e8258c63a3e0174ecce64
Rhian Castro <rhi.castro@gmail.com>
2021-02-09 21:21:16 -0300

    file process:process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for item in range(len(instance)):
+        if instance.search(item)["nome_do_arquivo"] == path_file:
+            return
+        
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
+    }
+    
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
