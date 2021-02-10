ee2f81da01e4f4bc01618632d2ec734a3ccf4ac3
Rhian Castro <rhi.castro@gmail.com>
2021-02-10 00:06:06 -0300

    there is a option called -Format document with > Python- that fixes linter issues and now I feel dumb for doing it by hand...

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,46 @@
+import re
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    lines = 0
+    result = []
+    ocorrences = []
+    for words in range(len(instance)):
+        lines += 1
+        temp = instance.search(words)
+        for line in temp["linhas_do_arquivo"]:
+            if re.findall(word, line, re.IGNORECASE):
+                line_number = lines
+                ocorrences.append({"linha": line_number})
+                result.append({
+                    "palavra": f"{word}",
+                    "arquivo": temp["nome_do_arquivo"],
+                    "ocorrencias": ocorrences
+                })
+    return result
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    result = []
+    ocorrences = []
+    lines = 0
+    for worlds in range(len(instance)):
+        lines += 1
+        temp = instance.search(worlds)
+        for line in temp["linhas_do_arquivo"]:
+            if re.findall(word, line, re.IGNORECASE):
+                line_number = lines
+                ocorrences.append({
+                    "linha": line_number,
+                    "conteudo": f"{line}"
+                })
+                result.append(
+                    {
+                        "palavra": f"{word}",
+                        "arquivo": temp["nome_do_arquivo"],
+                        "ocorrencias": ocorrences,
+                    }
+                )
+        return result
