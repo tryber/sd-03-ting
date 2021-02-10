2fcd66cebb847c9e579a5af2d6a69925d7b52e9d
renandreolle@hotmail.com <renandreolle@hotmail.com>
2021-02-10 08:53:14 -0300

    process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+
+        with open(path_file, "r") as file:
+            text_data = []
+            text_lines = file.readlines()
+            for line in text_lines:
+                text_data.append(line.strip())
+            return text_data
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    for i in range(instance.__len__()):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return
+
+    retorno = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+    instance.enqueue(retorno)
+    print(retorno, file=sys.stdout)
 
 
 def remove(instance):
