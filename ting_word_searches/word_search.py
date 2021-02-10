a593c7bed10b678227b0f494088719ec90595c0b
renandreolle@hotmail.com <renandreolle@hotmail.com>
2021-02-10 11:36:20 -0300

    re

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocurrences = []
+    lister = []
+    for i in range(len(instance)):
+        for line in instance.search(i)["linhas_do_arquivo"]:
+            if re.search(word, line, re.IGNORECASE):
+                ocurrences.append({"linha": i + 1})
+                lister.append(
+                    {
+                        "palavra": f"{word}",
+                        "arquivo": instance.search(i)["nome_do_arquivo"],
+                        "ocorrencias": ocurrences,
+                    }
+                )
+    return lister
 
 
 def search_by_word(word, instance):
